9fc0d3f70645fcf3a813b97ab06cabd3c60a2443
lukaszhandy <66007288+lukaszhandy@users.noreply.github.com>
2021-01-12 10:38:09 +0100

    Update UPDreset.ps1

--- UPDreset.ps1
+++ UPDreset.ps1
@@ -1,8 +1,8 @@
-New-EventLog –LogName Application –Source 'UPDKZGM' -ErrorAction SilentlyContinue
+New-EventLog –LogName Application –Source 'UPD' -ErrorAction SilentlyContinue
 while(Get-PnpDevice -InstanceId "USB\VID_03F0&PID_3917\5*" -Status "Error" -ErrorAction SilentlyContinue){
-Write-EventLog -LogName "Application" -Source "UPDKZGM" -EventID 7210 -EntryType Warning -Message "Urządzenie USB\VID_03F0&PID_3917 wymaga inicjalizacji." -Category 1 -RawData 10,20
+Write-EventLog -LogName "Application" -Source "UPD" -EventID 7210 -EntryType Warning -Message "Urządzenie USB\VID_03F0&PID_3917 wymaga inicjalizacji." -Category 1 -RawData 10,20
 Get-PnpDevice -InstanceId "USB\VID_03F0&PID_3917\5*" -Status "Error" -ErrorAction SilentlyContinue| Disable-PnpDevice -Confirm:$false -ea SilentlyContinue
 sleep(10)
 Get-PnpDevice -InstanceId "USB\VID_03F0&PID_3917\5*" -Status "Error" -ErrorAction SilentlyContinue | enable-PnpDevice -Confirm:$false -ea SilentlyContinue
 }
-Write-EventLog -LogName "Application" -Source "UPDKZGM" -EventID 7210 -EntryType Information -Message "Urządzenie USB\VID_03F0&PID_3917 zostało poprawnie zainicjowane." -Category 1 -RawData 10,20
+Write-EventLog -LogName "Application" -Source "UPD" -EventID 7210 -EntryType Information -Message "Urządzenie USB\VID_03F0&PID_3917 zostało poprawnie zainicjowane." -Category 1 -RawData 10,20
